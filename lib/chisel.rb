30ee90792cd16ce821af6f12727c16add06182f7
Matt Rooney <matthewjrooney@gmail.com>
2015-09-02 21:09:36 -0600

    added passing header tests, no edge cases

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -1,22 +1,63 @@
 
-  def parse_h1(string)
-    if string.include? "# "
-      string = string.delete "# "
-      "<h1>#{string}</h1>"
-    else
-      string
+  def parse_h1(h1)
+      if h1[0].include? "#"
+        h1 = h1.delete "# "
+        "<h1>#{h1}</h1>"
+      else "<p>#{h1}</p>"
+      end
+  end
+
+  def parse_h2(h2)
+    if h2[0..1].include? "##"
+      h2 = h2.delete "## "
+      "<h2>#{h2}</h2>"
     end
-  end                            # => :parse_h1
+  end
 
-  def parse_h2(string)
-    string = string.delete "## "
-    "<h2>#{string}</h2>"
-  end                             # => :parse_h2
+  def parse_h3(h3)
+    if h3[0..2].include? "###"
+      h3 = h3.delete "### "
+      "<h3>#{h3}</h3>"
+    end
+  end
 
-  def parse_h3(string)
-    string = string.delete "### "
-    "<h3>#{string}</h3>"
-  end                              # => :parse_h3
+  def parse_h4(h4)
+    if h4[0..3].include? "####"
+      h4 = h4.delete "#### "
+      "<h4>#{h4}</h4>"
+    end
+  end
+
+  def parse_h5(h5)
+    if h5[0..4].include? "#####"
+      h5 = h5.delete "##### "
+      "<h5>#{h5}</h5>"
+    end
+  end
+
+  def parse_h6(h6)
+    if h6[0..5].include? "######"
+      h6 = h6.delete "###### "
+      "<h6>#{h6}</h6>"
+    end
+  end
 
 # text = "This is not a header"
 #parse_h1(text)
+
+
+#   def parse_header
+#       if string[5]
+#         parse_h5
+#       elsif string[6]
+#         parse_h6
+#
+#       else
+#     end
+#   end
+#
+
+# end_with?
+# lines(separator=$/)
+# same as str.each_line(separator).to_a
+# line_array = line.split
--- test/chisel_test.rb
+++ test/chisel_test.rb
@@ -1,20 +1,15 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require_relative 'chisel'
+require_relative '../lib/chisel'
 
 class ChiselTest < Minitest::Test
   def test_parse_h1
-      header = parse_h1("# hello")
-      assert_equal "<h1>hello</h1>", header
-  end
-
-  def test_parse_h1_ignores_two_hashtags
-    header = parse_h1("## hello")
-    assert_equal "## hello", header
+    header = parse_h1("# hello")
+    assert_equal "<h1>hello</h1>", header
   end
 
   def test_parse_h2
-    header = parse_h2("# hello")
+    header = parse_h2("## hello")
     assert_equal "<h2>hello</h2>", header
   end
 
@@ -22,4 +17,25 @@ class ChiselTest < Minitest::Test
     header = parse_h3("### hello")
     assert_equal "<h3>hello</h3>", header
   end
+
+  def test_parse_h4
+    header = parse_h4("#### hello")
+    assert_equal "<h4>hello</h4>", header
+  end
+
+  def test_parse_h5
+    header = parse_h5("##### hello")
+    assert_equal "<h5>hello</h5>", header
+  end
+
+  def test_parse_h6
+    header = parse_h6("###### hello")
+    assert_equal "<h6>hello</h6>", header
+  end
+
 end
+
+# def test_parse_h1_ignores_two_hashtags
+#   header = parse_h1("## hello")
+#   assert_equal "## hello", header
+# end
